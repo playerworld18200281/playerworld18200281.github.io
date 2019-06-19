0ac139f7a9920c4a976f7e4828314d090e95c27d
xsk <xushenkai@ylb.net>
2019-06-20 03:01:51 +0800

    test

--- new_project.sh
+++ new_project.sh
@@ -3,21 +3,23 @@
 
 git_new_project(){
 
-    con_show_str="[提示信息:]";
+    con_show_str="/---提示信息---/"
 
-    rm -rf /var/www/playerworld18200281.github.io
+    project_name="playerworld18200281.github.io"
 
-    echo "$con_show_str 删除playerworld18200281.github.io 完成";
+    rm -rf "/var/www/$project_name"
+
+    echo "$con_show_str 删除playerworld18200281.github.io 完成"
 
     cd /var/www
 
     git clone git@github.com:playerworld18200281/playerworld18200281.github.io.git
 
-    echo "$con_show_str clone playerworld18200281.github.io 执行完成";
+    echo "$con_show_str 克隆$project_name 执行完成"
 
     chmod -R 777 /var/www/playerworld18200281.github.io
 
-    echo "$con_show_str playerworld18200281 777 权限执行完成";
+    echo "$con_show_str $project_name 权限777执行完成"
 }
 
 echo "-----函数开始执行-----"
