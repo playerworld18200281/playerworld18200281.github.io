92ff26b7e960576369798bb1f29904fca954121b
xsk <xushenkai@ylb.net>
2019-06-19 21:27:38 +0800

    test

--- contracts/eosio.contracts/eosio.token/include/eosio.token/eosio.token.hpp
+++ contracts/eosio.contracts/eosio.token/include/eosio.token/eosio.token.hpp
@@ -24,10 +24,10 @@ namespace eosio {
          using contract::contract;
 
        [[eosio::action]]
-       void sendsq( name from ,name to, asset amount, string memo);
+       void sends( name from ,name to, asset amount, string memo);
        
        [[eosio::on_notify("eosio.token::transfer")]]
-       void ontransferq( name from ,name to, asset amount, string memo);
+       void ontransfer( name from ,name to, asset amount, string memo);
        
        //------------- start -------------//
 //       [[eosio::on_notify("eosio::onerror")]]
