24c9fda5b46ebf534ebbf51841b199fe2753521a
xsk <xushenkai@ylb.net>
2019-06-26 17:00:59 +0800

    脚本自动提交 2019年 6月26日 星期三 17时00分59秒 CST

--- contract.sh
+++ contract.sh
@@ -8,8 +8,8 @@ printf "\033c"
 cd "/opt/www"
 fun_exe_start="\033[42;34m 开始执行 `date` \033[0m"
 fun_exe_end="\033[42;34m 结束执行 `date` \033[0m"
-con_show_str_start="\033[46;30m【"
-con_show_str_end=" 执行完成】\033[0m"
+con_show_str_start="\033[46;30m【 执行 "
+con_show_str_end=" 完成】\033[0m"
 project_name="playerworld18200281.github.io"
 
 git_new_project(){
@@ -38,40 +38,39 @@ compile(){
   eosio-cpp -I include -o eosio.token.wasm src/eosio.token.cpp --abigen
   echo -e "$con_show_str_start 编译.wasm .cpp $con_show_str_end"
   cleos -u http://jungle2.cryptolions.io set contract xushenkai123 /opt/www/playerworld18200281.github.io/eosio.token -p xushenkai123@active
-  echo -e "$con_show_str_start 执行set contract $con_show_str_end"
+  echo -e "$con_show_str_start set contract $con_show_str_end"
 
   echo -e "$con_show_str_start sendms $con_show_str_end"
   cleos -u http://jungle2.cryptolions.io  push action xushenkai123 sendms '["xushenkai123", "for 600 seconds", 600]' -p xushenkai123
 }
 
-contract(){
+contract_exe(){
 
 
 
-    echo -e "$con_show_str_start 执行testcheck $con_show_str_end"
+    echo -e "$con_show_str_start testcheck $con_show_str_end"
     cleos -u http://jungle2.cryptolions.io push action xushenkai123 testcheck '[""]' -p xushenkai123@active
 
-    echo -e "$con_show_str_start 执行adduser $con_show_str_end"
+    echo -e "$con_show_str_start adduser $con_show_str_end"
     cleos -u http://jungle2.cryptolions.io push action xushenkai123 adduser '["xushenkai123", 13888888888, "im bob"]' -p xushenkai123@active
 
 
-    echo -e "$con_show_str_start 执行get table user $con_show_str_end"
+    echo -e "$con_show_str_start get table user $con_show_str_end"
     cleos -u http://jungle2.cryptolions.io get table xushenkai123 xushenkai123 user
 
 
-    echo -e "$con_show_str_start 执行addpost $con_show_str_end"
+    echo -e "$con_show_str_start addpost $con_show_str_end"
     cleos -u http://jungle2.cryptolions.io push action xushenkai123 addpost '["xushenkai123", "my post"]' -p xushenkai123
 
-    echo -e "$con_show_str_start 执行get table post $con_show_str_end"
+    echo -e "$con_show_str_start get table post $con_show_str_end"
     cleos -u http://jungle2.cryptolions.io get table xushenkai123 xushenkai123 post
 
 }
 
 echo -e $fun_exe_start
-
 git_pull
 compile
-contract
+contract_exe
 echo -e $fun_exe_end
 
 
